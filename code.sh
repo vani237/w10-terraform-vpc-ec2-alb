afb67b3a3ebc660c6c23102a53c6813dc975852d
vani237 <awsprocerti@gmail.com>
2024-03-19 19:52:28 -0500

    new code

---
+++ code.sh
@@ -0,0 +1,5 @@
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+echo "<html><h1><p> Welcome to Utrains the place to learn DevOps!!.<br> This traffic is servered from:  ${HOSTNAME} </p></h1></html>" > /var/www/html/index.html
